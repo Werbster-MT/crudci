0ecd5e5606104dc4ffdb3c8735ab69b777f765fb
Werbster <wmteixeira.88@gmail.com>
2024-07-24 13:54:09 -0300

    [FEAT] Finalizando autenticacao de usuarios.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,9 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
-	public $login_model;
-	public $session;
-
 	public function __construct()
 	{
 		parent::__construct();
--- application/controllers/Signup.php
+++ application/controllers/Signup.php
@@ -1,20 +1,22 @@
 <?php
-error_reporting(0);
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Signup extends CI_Controller {
+class Signup extends CI_Controller
+{
+
 	public function index()
-	{   
-		$data['title'] = "Sign-up - CodeIgniter.";
-		$this->load->view('pages/signup', $data);
+	{
+		$dados["title"] = "Sign Up - CodeIgniter";
+		$this->load->view('pages/signup', $dados);
 	}
 
-	public function store() {
+	public function store()
+	{
 		$this->load->model("users_model");
 		$user = array(
 			"name" => $_POST["name"],
-			"country" => $this->input->post("country"),
-			"email" => $_POST["password"],
+			"country" => $_POST["country"],
+			"email" => $_POST["email"],
 			"password" => md5($_POST["password"])
 		);
 		$this->users_model->store($user);
